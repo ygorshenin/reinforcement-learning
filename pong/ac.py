[pong] Fixes to gradient updates.

pong/ac.py
@@ -1,6 +1,7 @@
 import collections
 import numpy as np
 import random
+import tensorflow as tf
 
 from env import Env
 from policy_dqn import PolicyDQN
@@ -20,43 +21,48 @@ def clamp(value, low, high):
 
 
 class AC:
-    def __init__(self):
+    def __init__(self, discount):
         self.value = ValueDQN(input_shape=Env.observations_shape())
         self.policy = PolicyDQN(input_shape=Env.observations_shape())
         self.memory = collections.deque(maxlen=MEMORY_SIZE)
+        self.discount = discount
 
     def get_action_prob(self, sess, state):
         p = self.policy.predict(sess, state)
         a = 1 if np.random.random() < p else 0
         return a, p
 
+    def get_action(self, sess, state):
+        a, _ = self.get_action_prob(sess, state)
+        return a
+
     def get_value(self, sess, state):
         return self.value.predict(sess, state)
 
-    def on_reward(self, s, a, p, r, s_, done):
-        self.memory.append([s, a, p, r, s_, done])
+    def on_reward(self, s, a, r, s_, done):
+        self.memory.append([s, a, r, s_, done])
 
     def train(self, sess, lr_policy, lr_value):
         batch_size = min(len(self.memory), BATCH_SIZE)
         samples = random.sample(self.memory, batch_size)
 
-        ss, ss_ = [], []
-        for [s, a, p, r, s_, done] in samples:
+        ss, as_, ss_ = [], [], []
+        for [s, a, r, s_, done] in samples:
             ss.append(s)
+            as_.append(a)
             ss_.append(s_)
 
         values_pred = self.value.predict_on_batch(sess, ss)
         values_pred_ = self.value.predict_on_batch(sess, ss_)
+        as_ = np.expand_dims(np.array(as_), axis=1)
 
         values, weights = np.zeros(shape=[batch_size, 1]), np.zeros(shape=[batch_size, 1])
-        for i, [s, a, p, r, _s, done] in enumerate(samples):
+        for i, [s, a, r, _s, done] in enumerate(samples):
             reward = r
             if not done:
-                reward += values_pred_[i]
+                reward += self.discount * values_pred_[i][0]
             values[i][0] = reward
-            if a == 1:
-                weights[i][0] = (reward - values_pred[i]) / clamp(p, EPS, 1 - EPS)
-            else:
-                weights[i][0] = (values_pred[i] - reward) / clamp(1 - p, EPS, 1 - EPS)
-        self.policy.train_on_batch(sess, ss, weights, lr_policy)
+            weights[i][0] = reward - 0.5 * values_pred[i][0]
+
+        self.policy.train_on_batch(sess, ss, as_, weights, lr_policy)
         self.value.train_on_batch(sess, ss, values, lr_value)

pong/env.py
@@ -7,11 +7,11 @@ MAX_FRAMES = 2
 
 MIN_ROW = 34
 MAX_ROW = 193
-NROWS = MAX_ROW - MIN_ROW
+NROWS = (MAX_ROW - MIN_ROW) // 2
 
 MIN_COL = 0
 MAX_COL = 160
-NCOLS = MAX_COL - MIN_COL
+NCOLS = (MAX_COL - MIN_COL) // 2
 
 
 class Env:
@@ -27,29 +27,31 @@ class Env:
             time.sleep(0.02)
 
         self.f0, self.f1 = frame, frame
-        self.state = np.hstack([self.f0, self.f1])
-
-        return self.state
+        return self.make_state()
 
     def step(self, action):
         frame, reward, done, _ = self.env.step(2 + action)
         frame = Env._normalize_frame(frame)
 
         self.f0, self.f1 = self.f1, frame
-        self.state = np.hstack([self.f0, self.f1])
+        state = self.make_state()
 
         if self.render:
             self.env.render()
             time.sleep(0.02)
-        return self.state, reward, done
+        return state, reward, done
 
     @staticmethod
     def observations_shape():
-        return [NROWS, MAX_FRAMES * NCOLS, 1]
+        return [1, 80, 160]
 
     @staticmethod
     def _normalize_frame(frame):
         frame = frame[MIN_ROW:MAX_ROW, :, 0]
         frame[frame == 144] = 0
         frame[frame != 0] = 1
-        return frame.reshape([NROWS, NCOLS, 1])
+        return frame[::2, ::2]
+
+    def make_state(self):
+        state = np.hstack([self.f0, self.f1])
+        return np.expand_dims(state, axis=0)

pong/learn.py
@@ -8,17 +8,17 @@ from env import Env
 
 
 REWARD_DECAY = 0.99
+DISCOUNT = 1
 
 
 def train_on_episode(sess, env, ac):
-    s = env.reset()
-    reward = 0
+    s, reward = env.reset(), 0
 
     while True:
-        a, p = ac.get_action_prob(sess, s)
+        a = ac.get_action(sess, s)
         s_, r, done = env.step(a)
         d = abs(r) > 1e-5
-        ac.on_reward(s, a, p, r, s_, d)
+        ac.on_reward(s, a, r, s_, d)
         s = s_
 
         if d:
@@ -27,7 +27,7 @@ def train_on_episode(sess, env, ac):
                 print('Win :)')
             else:
                 print('Loss :(')
-            ac.train(sess, lr_policy=1e-4, lr_value=1e-4)
+            ac.train(sess, lr_policy=1e-3, lr_value=1e-3)
 
         if done:
             break
@@ -42,11 +42,13 @@ def train_on_episodes(args):
 
     with tf.Session(config=config) as sess:
         env = Env(render=False)
-        ac = AC()
+        ac = AC(discount=DISCOUNT)
 
         sess.run(tf.global_variables_initializer())
 
         writer = tf.summary.FileWriter(args.summary, sess.graph)
+        ac.writer = writer
+
         saver = tf.train.Saver()
 
         if args.restore:

pong/policy_dqn.py
@@ -13,20 +13,28 @@ class PolicyDQN:
         self.states = tf.placeholder(tf.float32, [None] + list(input_shape), name='states')
         self.weights = tf.placeholder(tf.float32, [None, 1], name='weights')
         self.learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+        self.actions = tf.placeholder(tf.float32, [None, 1], name='actions')
 
-        h1 = tf.layers.dense(inputs=tf.layers.flatten(self.states),
-                             units=512,
+        conv1 = tf.layers.conv2d(inputs=self.states,
+                                 filters=64,
+                                 kernel_size=8,
+                                 strides=4,
+                                 data_format='channels_first',
+                                 activation=tf.nn.tanh,
+                                 kernel_initializer=xavier_initializer())
+        h1 = tf.layers.dense(inputs=tf.layers.flatten(conv1),
+                             units=128,
                              activation=tf.nn.tanh,
                              kernel_initializer=xavier_initializer())
-        h2 = tf.layers.dense(inputs=h1,
-                             units=hidden_units,
-                             activation=tf.nn.tanh,
-                             kernel_initializer=xavier_initializer())
-        self.probs = tf.layers.dense(inputs=h2,
-                                     units=1,
-                                     activation=tf.sigmoid,
-                                     kernel_initializer=xavier_initializer())
-        loss = -tf.reduce_mean(self.probs * self.weights)
+        self.prob_1 = tf.layers.dense(inputs=h1,
+                                      units=1,
+                                      activation=tf.sigmoid,
+                                      kernel_initializer=xavier_initializer())
+        self.prob_1 = self.prob_1 * 0.9998 + 0.0001
+        self.prob_0 = 1 - self.prob_1
+
+        self.log_prob = tf.log(self.actions * self.prob_1 + (1 - self.actions) * (1 - self.prob_1))
+        loss = tf.reduce_mean(self.log_prob * self.weights)
 
         optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
         self.train_op = optimizer.minimize(loss)
@@ -35,9 +43,10 @@ class PolicyDQN:
         return self.predict_on_batch(sess, [state])[0][0]
 
     def predict_on_batch(self, sess, states):
-        return sess.run(self.probs, feed_dict={self.states: states})
+        return sess.run(self.prob_1, feed_dict={self.states: states})
 
-    def train_on_batch(self, sess, states, weights, learning_rate):
+    def train_on_batch(self, sess, states, actions, weights, learning_rate):
         return sess.run(self.train_op, feed_dict={self.states: states,
+                                                  self.actions: actions,
                                                   self.weights: weights,
                                                   self.learning_rate: learning_rate})

pong/value_dqn.py
@@ -12,23 +12,26 @@ class ValueDQN:
         self.values_true = tf.placeholder(tf.float32, shape=[None, 1], name='values_true')
         self.learning_rate = tf.placeholder(tf.float32, name='learning_rate')
 
-        h1 = tf.layers.dense(inputs=tf.layers.flatten(self.states),
-                             units=512,
+        conv1 = tf.layers.conv2d(inputs=self.states,
+                                 filters=64,
+                                 kernel_size=8,
+                                 strides=4,
+                                 data_format='channels_first',
+                                 activation=tf.nn.tanh,
+                                 kernel_initializer=xavier_initializer())
+        h1 = tf.layers.dense(inputs=tf.layers.flatten(conv1),
+                             units=128,
                              activation=tf.nn.tanh,
                              kernel_initializer=xavier_initializer())
-        h2 = tf.layers.dense(inputs=h1,
-                             units=hidden_units,
-                             activation=tf.nn.tanh,
-                             kernel_initializer=xavier_initializer())
-        self.values_pred = tf.layers.dense(inputs=h2,
+        self.values_pred = tf.layers.dense(inputs=h1,
                                            units=1,
                                            kernel_initializer=xavier_initializer())
 
-        loss = tf.losses.mean_squared_error(self.values_true, self.values_pred)
-        loss = tf.reduce_mean(loss)
+        self.loss = tf.losses.mean_squared_error(self.values_true, self.values_pred)
+        self.loss = tf.reduce_mean(self.loss)
 
         optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-        self.train_op = optimizer.minimize(loss)
+        self.train_op = optimizer.minimize(self.loss)
 
     def predict(self, sess, state):
         return self.predict_on_batch(sess, [state])[0][0]
@@ -40,4 +43,4 @@ class ValueDQN:
         feed_dict = {self.states: states,
                      self.values_true: values_true,
                      self.learning_rate: learning_rate}
-        sess.run(self.train_op, feed_dict=feed_dict)
+        return sess.run([self.train_op, self.loss], feed_dict=feed_dict)

pong/visualize.py
@@ -33,7 +33,7 @@ def visualize_episodes(sess, env, ac):
 def go(args):
     with tf.Session() as sess:
         env = Env(render=True)
-        ac = AC()
+        ac = AC(discount=1)
 
         saver = tf.train.Saver()
         saver.restore(sess, args.model_path)